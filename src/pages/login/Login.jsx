6a98800c2d15581f5b7fdfc9ed669ca3f7a84908
Ismaila Saidu <ismailasaidu997@gmail.com>
2023-04-13 16:42:02 -0400

    admin panel and animation

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import axios from "axios";
+
 import { useNavigate } from "react-router-dom";
 
 import { collection, getDocs } from "firebase/firestore";
